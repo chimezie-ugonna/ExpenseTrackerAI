6e024c4821471dfedac1e329cd3c20c27b2a6353
Ugonna Chimezie <ugiezie@gmail.com>
2025-03-21 18:31:17 +0100

    Set up basic Spring Boot application structure with Maven and initial dependencies

---
+++ src/main/java/com/expensetrackerai/ExpenseTrackerAiApplication.java
@@ -0,0 +1,11 @@
+package com.expensetrackerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerAiApplication.class, args);
+    }
+}
